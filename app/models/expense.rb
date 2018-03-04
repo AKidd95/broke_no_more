19e3d2d91d21e98e23827a0dc51b1bb0aa414a2f
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-03-04 17:22:36 +0100

    add some calculations to model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,20 @@
 class Expense < ApplicationRecord
+
+  def total_expenses
+    binding.pry
+    Expense.where(id: user_id).sum('rent + insurance + transportation + education + food +
+                                    other + leisure_activities + cultural_activities +
+                                    holidays + eating_out + sport + beauty + various')
+    return total_expenses
+  end
+
+  def income
+    income = Income.new(amount)
+  end
+
+  def remaining_money
+    income - total_expenses
+    return remaining_money
+  end
+
 end
